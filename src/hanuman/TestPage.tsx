da2ee956dacda39e239e02ba2155600735eb4eed
Hanuman Ghawade <hanuman.ghawade@excellarate.com>
2023-02-07 18:28:24 +0530

    "file updated"

--- src/hanuman/TestPage.tsx
+++ src/hanuman/TestPage.tsx
@@ -4,15 +4,6 @@ import axios from 'axios';
 export default function TestPage() {
   const [testData, setTestData] = useState('');
 
-  useEffect(() => {
-    const getSomeData = async () => {
-      const res = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-      console.log(res.data);
-      setTestData(res.data[0]?.title);
-    };
-    getSomeData();
-  }, []);
-
   return (
     <div className="test">
       <h1>{testData}</h1>
